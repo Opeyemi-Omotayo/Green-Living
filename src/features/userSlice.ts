22bc57ea9a483ead63d9786b57a7be18ef18aee8
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-23 13:51:23 +0100

    feat:created a user slice

--- src/features/userSlice.ts
+++ src/features/userSlice.ts
@@ -1,3 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-export {}
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    isSignedIn: false,
+    userData: null,
+    searchInput: "tech",
+    blogData: null,
+  },
+  reducers: {
+    setSignedIn: (state, action) => {
+      state.isSignedIn = action.payload;
+    },
+    setUserData: (state, action) => {
+      state.userData = action.payload;
+    },
+    setInput: (state, action) => {
+      state.searchInput = action.payload;
+    },
+    setBlogData: (state, action) => {
+      state.blogData = action.payload;
+    },
+  },
+});
+
+export const {
+  setSignedIn,
+  setUserData,
+  setInput,
+  setBlogData,
+} = userSlice.actions;
+
+export const selectSignedIn = (state: any) => state.user.isSignedIn;
+export const selectUserData = (state : any) => state.user.userData;
+export const selectUserInput = (state: any) => state.user.searchInput;
+export const selectBlogData = (state: any) => state.user.blogData;
+
+export default userSlice.reducer;
