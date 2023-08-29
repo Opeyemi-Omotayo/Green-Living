b9316cd2036a1905629a91f72af32633db30af33
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-29 20:36:01 +0100

    wip

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -10,7 +10,6 @@ const Homepage = () => {
 
   const dispatch = useDispatch();
   const login = (response: any) => {
-    console.log(response);
     dispatch(setSignedIn(true));
     dispatch(setUserData(response.profileObj));
   };
