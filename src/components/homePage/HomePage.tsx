bd4d0a2aede52e09f87b23ae50b789ae27c1d709
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-30 17:50:39 +0100

    UI: reduced font size for mobile devices

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -31,7 +31,7 @@ const Homepage = () => {
           <div className=" flex flex-col items-center w-[90%] md:[80%] lg:w-[60%] ">
             <h2 className="text-5xl text-green-500"><FaAudible /></h2>
             <h1 className="my-3 text-xl">Welcome to Green Living!</h1>
-            <p className="mb-[2rem]">
+            <p className="mb-[2rem] text-sm md:text-lg">
               This is a platform where we share insightful articles, stories, and experiences on a wide range of topics. Whether you're interested in technology, lifestyle, travel, or personal growth, you'll find something engaging and thought-provoking here.
             </p>
 
