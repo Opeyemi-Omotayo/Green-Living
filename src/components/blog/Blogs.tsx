c255d9aac8f2310edcf8478be31c95efa3f63f92
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-30 16:50:04 +0100

    perf: cleaning up

--- src/components/blog/Blogs.tsx
+++ src/components/blog/Blogs.tsx
@@ -17,9 +17,10 @@ interface Blog {
 
 const Blogs = () => {
     const searchInput = useSelector(selectUserInput);
-    const blog_url = `https://gnews.io/api/v4/search?q=${searchInput}&token=${process.env.REACT_APP_GNEWS_API_KEY}`;
     const dispatch = useDispatch();
     const [blogs, setBlogs] = useState<any>(null);;
+
+    const blog_url = `https://gnews.io/api/v4/search?q=${searchInput}&country=ng&token=${process.env.REACT_APP_GNEWS_API_KEY}`;
 
     const [loading, setLoading] = useState(true);
 
@@ -34,15 +35,15 @@ const Blogs = () => {
             .catch((error) => {
                 toast(error);
             });
-    }, [searchInput]);
+    }, [searchInput, blog_url]);
 
     return (
         <div className="font-mono px-[3rem]">
             <h1 className="pt-[3rem] text-4xl underline">Blogs</h1>
-            {loading ? <h1 >Loading...</h1> : ""}
+            {loading ? <h1>Loading...</h1> : ""}
             <div className="  grid gap-10 pt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:place-content-center">
                 {blogs?.articles?.map((blog: Blog) => (
-                    <a className="shadow-lg bg-gray-50 border rounded-md pb-5" target="_blank" key={blog.title} href={blog.url}>
+                    <a className="shadow-lg bg-gray-50 border rounded-md pb-5" target="_blank" rel="noreferrer" key={blog.title} href={blog.url}>
                         <img src={blog.image} className="h-[180px] w-full" alt={blog.title} />
                         <div>
                             <h3 className="flex text-sm items-center justify-between p-3" >
@@ -55,7 +56,7 @@ const Blogs = () => {
                     </a>
                 ))}
 
-                {blogs?.totalArticles == 0 && (
+                {blogs?.totalArticles === 0 && (
                     <h1 className="flex items-center justify-center">
                         No blogs available 😞. Search something else to read blogs on the
                         greatest platform.
--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -25,7 +25,7 @@ const Homepage = () => {
 
   return (
     <div className={` ${isSignedIn ? 'hidden' : 'block'}`}>
-      <div className="font-mono flex items-center justify-center h-[100vh]">
+      <div className="font-mono flex items-center justify-center h-[90vh]">
         {!isSignedIn ? (
           <div className=" flex flex-col items-center w-[60%] ">
             <h2 className="text-5xl">📗</h2>
