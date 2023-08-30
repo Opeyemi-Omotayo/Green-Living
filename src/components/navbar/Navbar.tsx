de9433c0610d8e9aeafefae61da22dabdf55541a
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-30 14:08:20 +0100

    UI: designed navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,30 +6,29 @@ const Navbar = () => {
   const isSignedIn = useSelector(selectSignedIn);
 
   return (
-    <div className="flex">
+    <div className="flex items-center justify-between bg-black text-white py-3 px-[3rem]">
       <h1 >Green Living</h1>
       {isSignedIn ? (
-        <div >
+        <div className="flex flex-1 mx-[3rem]">
           <input
             placeholder="Search for a blog"
+            className="p-2 flex-1 text-black outline-none"
           />
-          <button>
+          <button className="bg-green-500 p-2">
             Search
           </button>
         </div>
       ): ("")}
 
       {isSignedIn ? (
-        <div >
-          
-          <h1 >user</h1>
-         
-              <button>
+        <div className="flex bg-gray-900 py-2 px-4 rounded-lg">
+          <h1 className="mr-5" >User</h1>
+              <button className="bg-gray-800 py-1 px-2 rounded-lg" >
                 Logout 😦
               </button>
         </div>
       ) : (
-        <h1 >No User available 😞</h1>
+        <h1 className="bg-gray-900 py-2 px-4 rounded-lg" >No User available 😞</h1>
       )}
     </div>
   );
