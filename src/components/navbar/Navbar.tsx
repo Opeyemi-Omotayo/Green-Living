76715af7746d086a3baf7d2b1a417da4a5b17e19
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-30 14:27:02 +0100

    feat: search functionality

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { selectSignedIn } from "../../features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectSignedIn, setInput } from "../../features/userSlice";
 
 const Navbar = () => {
-  const isSignedIn = useSelector(selectSignedIn);
-
+    const [inputValue, setInputValue] = useState("tech");
+    const isSignedIn = useSelector(selectSignedIn);
+  
+    const dispatch = useDispatch();
+  
+    const handleClick = (e:any) => {
+      e.preventDefault();
+      dispatch(setInput(inputValue));
+    };
+  
   return (
     <div className="flex items-center justify-between bg-black text-white py-3 px-[3rem]">
       <h1 >Green Living</h1>
@@ -13,8 +21,10 @@ const Navbar = () => {
           <input
             placeholder="Search for a blog"
             className="p-2 flex-1 text-black outline-none"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
-          <button className="bg-green-500 p-2">
+          <button className="bg-green-500 p-2" onClick={handleClick}>
             Search
           </button>
         </div>
